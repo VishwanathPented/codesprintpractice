15e90111742dea84180ad9ce7116f31011106f86
Vishwanath Pented <vishupented04@gmail.com>
2026-03-18 15:43:12 +0530

    Implement basic arithmetic operations in Day2.java
    Prints the sum, product, and remainder of two integers.

---
+++ Day2.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+    int a = 15;
+    int b = 4;
+    // Print sum, product, and remainder
+    System.out.println((a+b) + " " + (a*b) + " " + (a%b));
+  }
+}
